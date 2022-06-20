356c2767487d91d481cb0b7270f6a09c6d3df8c1
Joshua Morgan <JoshuaReubenMorgan@gmail.com>
2022-06-20 20:19:01 +0100

    Fixing Login

--- GymBuddies/Data/Migrations/20220619181724_Test.Designer.cs
+++ GymBuddies/Data/Migrations/20220619181724_Test.Designer.cs
@@ -4,6 +4,7 @@ using GymBuddies.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymBuddies.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220619181724_Test")]
+    partial class Test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
